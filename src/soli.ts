9165cf1a42d968c029504271b612eb39c125c771
idlerus <lukas.voborsky@wedos.com>
2022-09-07 15:50:45 +0200

    few fixes

--- src/soli.ts
+++ src/soli.ts
@@ -5,21 +5,37 @@ type c = {'type': string, 'c': string, 'hidden': boolean};
 class Soli
 {
   private table: HTMLDivElement;
+  private window: HTMLDivElement;
 
   private pack: string[] = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-  private type: string[] = ['RD', 'RH', 'BL', 'BD'];
-  private sCount: number = 2;
+  private type: string[] = ['RH', 'RD', 'BL', 'BD'];
+  private sCount: number;
   private set: c[] = [];
 
+  private typesCount: number;
+
   private win: boolean = false;
+  private lose: boolean = false;
 
   private slots: {[slot: string]: c[]} = {};
-  private decks: c[] = [];
+
+  private hist: {action: string, data: any}[] = [];
+  private maxHist: number = 20;
 
   private selected: {slot: number|null, index: number|null} = {slot: null, index: null};
 
-  constructor(types: number = 1, decks: number = 2)
+  constructor(types: number = 1, packs: number = 2)
   {
+    this.typesCount = types;
+    this.sCount = packs;
+    this.gameSet(types);
+  }
+
+  public gameSet(types: number = 1): void
+  {
+    this.set = [];
+    this.slots = {};
+
     let multiplier = 1;
     if(![1,2,4].includes(types))
     {
@@ -42,20 +58,17 @@ class Soli
         for(let c of this.pack)
         {
           this.set.push(
-            {
-              'type': this.type[i],
-              'c': c,
-              'hidden': true,
-            }
+              {
+                'type': this.type[i],
+                'c': c,
+                'hidden': true,
+              }
           );
         }
       }
     }
     this.shuffle(this.set);
-  }
 
-  public gameSet(): void
-  {
     for(let i = 0; i < 10; i++)
     {
       this.slots[i] = [];
@@ -81,6 +94,7 @@ class Soli
   {
     this.rowSolved();
     this.gameWon();
+    this.checkLost();
 
     this.table.innerHTML = '';
 
@@ -101,10 +115,26 @@ class Soli
       this.table.appendChild(win);
     }
 
+    if(this.lose)
+    {
+      let lose = document.createElement('span');
+      lose.innerText = 'You lost!';
+      lose.classList.add('lose');
+      lose.style.position = 'absolute';
+      lose.style.top = '50%';
+      lose.style.left = '50%';
+      lose.style.transformOrigin = '50% 50%';
+      lose.style.background = '#fff';
+      lose.style.padding = '15px';
+      lose.style.fontSize = '32px';
+      lose.style.border = '3px solid #ddd';
+      lose.style.boxShadow = '0 10px 15px rgb(0 0 0 / 30%)';
+      this.table.appendChild(lose);
+    }
+
     for(let i in this.slots)
     {
       let slot = document.createElement('div');
-      console.log(this.slots[i].length);
       if(this.slots[i].length < 1)
       {
         let cDiv = document.createElement('div');
@@ -182,11 +212,39 @@ class Soli
 
   public renderG(): HTMLDivElement
   {
+    let window = document.createElement('div');
+    window.id = 'soliG';
+    this.window = window;
+
     let div = document.createElement('div');
-    div.id = 'soliG';
+    div.id = 'soliGTable';
     this.table = div;
+    this.window.appendChild(div);
+
+    let menu = document.createElement('div');
+    menu.id = 'soliGMenu';
+    let histButton = document.createElement('button');
+    let histListener = () => {
+      this.rollBackHist();
+    }
+    histButton.innerText = 'Undo';
+    histButton.addEventListener('click', histListener.bind(this));
+    menu.appendChild(histButton);
+
+    let restartButton = document.createElement('button');
+    let restartListener = () => {
+      this.gameSet(this.typesCount);
+      this.updateG();
+    }
+    restartButton.innerText = 'Restart game';
+    restartButton.addEventListener('click', restartListener.bind(this));
+    menu.appendChild(restartButton);
+
+    window.appendChild(menu);
+
+
     this.updateG();
-    return div;
+    return window;
   }
 
   private gameWon(): void
@@ -216,7 +274,15 @@ class Soli
       {
         if(c.type !== this.slots[slot][index + i].type)
         {
-          return false
+          return false;
+        }
+
+        if(this.slots[slot][index + i + 1] !== undefined)
+        {
+          if (this.slots[slot][index + i].c !== 'A' && this.slots[slot][index + i + 1].c !== this.pack[this.pack.indexOf(this.slots[slot][index + i].c) - 1])
+          {
+            return false;
+          }
         }
       }
     }
@@ -248,6 +314,7 @@ class Soli
               {
                 this.slots[i].splice(this.slots[i].length-13, 13);
                 this.slots[i][this.slots[i].length -1].hidden = false;
+                this.updateHist('rowComplete', {slotIn: i, type: c.type});
               }
             }
           }
@@ -268,6 +335,8 @@ class Soli
 
     if(this.slots[slotTarget].length < 1 || this.canPlace(sourceC, targetC))
     {
+      this.updateHist('move', {moveFromSlot: slot, moveFromIndex: index, moveToSlot: slotTarget, moveToIndex: this.slots[slotTarget].length});
+
       let moveIndex = this.slots[slot].length-index;
       let tempDeck = [];
 
@@ -296,6 +365,119 @@ class Soli
     }
   }
 
+  private updateHist(action: string, data: any): void
+  {
+    if(this.hist.length >= this.maxHist)
+    {
+      this.hist.shift();
+    }
+
+    this.hist.push({
+      action: action,
+      data: data,
+    });
+  }
+
+  private rollBackHist(): void
+  {
+    if(this.hist.length < 1)
+    {
+      return;
+    }
+
+    let action = this.hist.pop();
+
+    if(action.action === 'move')
+    {
+      let moveIndex = this.slots[action.data.moveToSlot].length-action.data.moveToIndex;
+      let tempDeck = [];
+
+      for(let i = 0; i < moveIndex; i++)
+      {
+        tempDeck.push(this.slots[action.data.moveToSlot].pop());
+      }
+
+      tempDeck = tempDeck.reverse();
+
+      for(let c of tempDeck)
+      {
+        this.slots[action.data.moveFromSlot].push(c);
+      }
+
+      if(this.slots[action.data.moveToSlot][action.data.moveToIndex-1] !== undefined && this.slots[action.data.moveToSlot][action.data.moveToIndex-1].hidden !== false)
+      {
+        this.slots[action.data.moveToSlot][action.data.moveToIndex-1].hidden = false;
+      }
+    }
+    else if(action.action === 'handout')
+    {
+      for(let i in this.slots)
+      {
+        let c = this.slots[i].pop();
+        this.set.push(c);
+      }
+    }
+    else if(action.action === 'rowComplete')
+    {
+      this.slots[action.data.slotIn].push(
+        {type: action.data.type, c: 'A', hidden: false},
+        {type: action.data.type, c: '2', hidden: false},
+        {type: action.data.type, c: '3', hidden: false},
+        {type: action.data.type, c: '4', hidden: false},
+        {type: action.data.type, c: '5', hidden: false},
+        {type: action.data.type, c: '6', hidden: false},
+        {type: action.data.type, c: '7', hidden: false},
+        {type: action.data.type, c: '8', hidden: false},
+        {type: action.data.type, c: '9', hidden: false},
+        {type: action.data.type, c: '10', hidden: false},
+        {type: action.data.type, c: 'J', hidden: false},
+        {type: action.data.type, c: 'Q', hidden: false},
+        {type: action.data.type, c: 'K', hidden: false},
+      );
+
+      this.rollBackHist();
+    }
+
+    this.updateG();
+  }
+
+  private checkLost(): void
+  {
+    let possibleMoves = 0;
+
+    if(this.set.length > 1)
+    {
+      return;
+    }
+
+    for(let i in this.slots)
+    {
+      for(let c of this.slots[i])
+      {
+        if(c.hidden === false)
+        {
+          for(let x in this.slots)
+          {
+            let targetC = this.slots[x][this.slots[x].length-1];
+            let sourceC = this.slots[i][this.slots[i].indexOf(c)];
+
+            if(this.canMove(parseInt(i), this.slots[i].indexOf(c)))
+            {
+              if(this.slots[x].length < 1 || this.canPlace(sourceC, targetC))
+              {
+                possibleMoves++;
+              }
+            }
+          }
+        }
+      }
+    }
+    if(possibleMoves < 1)
+    {
+      this.lose = true;
+    }
+  }
+
   private handOut(): boolean
   {
     let canDo = true;
@@ -320,6 +502,7 @@ class Soli
         c.hidden = false;
         this.slots[i].push(c);
       }
+      this.updateHist('handout', true);
       this.updateG();
       return true;
     }
